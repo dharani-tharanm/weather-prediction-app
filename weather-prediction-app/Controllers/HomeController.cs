f4c61fa1ba6a26808d753ad5e590ff3c52f1fd6e
dharani-tharanm <dharanitharan2019@gmail.com>
2024-07-17 22:38:45 +0530

    Redirected File

---
+++ weather-prediction-app/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace weather_prediction_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
